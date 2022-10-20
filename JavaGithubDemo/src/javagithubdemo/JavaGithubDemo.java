75d0b6e7559df138758108b511f66eef54d2a47b
hungvu268 <112080687+hungvu268@users.noreply.github.com>
2022-10-20 17:10:21 +0700

    Update JavaGithubDemo.java

--- JavaGithubDemo/src/javagithubdemo/JavaGithubDemo.java
+++ JavaGithubDemo/src/javagithubdemo/JavaGithubDemo.java
@@ -15,6 +15,7 @@ public class JavaGithubDemo {
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This is JavaGithubDemo");
     }
     
 }
